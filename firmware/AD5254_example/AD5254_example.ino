f88b2fe0329fcf6c5af98acaa191d2dda3390f73
Justin Culbertson <jculberts@gmail.com>
2019-04-05 13:06:49 -0700

    firmware v1 and board v2

---
+++ firmware/AD5254_example/AD5254_example.ino
@@ -0,0 +1,256 @@
+// Distributed with a free-will license.
+// Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+// AD5254
+// This code is designed to work with the AD5254_I2CPOT_10K I2C Mini Module available from ControlEverything.com.
+// https://www.controleverything.com/content/Potentiometers?sku=AD5254_I2CPOT_10K#tabs-0-product_tabset-2
+
+#include <Wire.h>
+#include <Encoder.h>
+
+// AD5254 I2C address is 0x2C(44)
+#define Addr 0x2F
+
+Encoder myEnc(1, 2);
+const int buttonPin = 0;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+void setup()
+{
+  
+  // Initialise I2C communication as Master
+  Wire.begin();
+  
+  // Initialise serial communication, set baud rate = 9600
+  Serial.begin(9600);
+  
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  setAllValues(0x00);
+  myEnc.write(0);
+}
+unsigned int volume = 0;
+long oldPosition  = -999;
+int ledState = LOW;         // the current state of the output pin
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+int registeredPress = 0;
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+void loop()
+{
+  readPushButtonState();
+  readRotaryEncoderState();
+}
+
+void readRotaryEncoderState() {
+  long newPosition = myEnc.read();
+  if (newPosition < 0) {
+    //Serial.print("<0 = ");
+    //Serial.println(newPosition);
+    newPosition = 0;
+    myEnc.write(0);
+    readAndPrintAllValues();
+  }
+  if (newPosition > 255) {
+    //Serial.print(">255 = ");
+    //Serial.println(newPosition);
+    newPosition = 255;
+    myEnc.write(255);
+    readAndPrintAllValues();
+  }
+  if (newPosition != oldPosition) {
+    unmute();
+    //Serial.print("mid = ");
+    //Serial.println(newPosition);
+    oldPosition = newPosition;
+    volume = newPosition;
+    setAllValues(volume);
+    readAndPrintAllValues();
+  }
+}
+
+void readPushButtonState() {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button 
+  // (i.e. the input went from LOW to HIGH),  and you've waited 
+  // long enough since the last press to ignore any noise:  
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  } 
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+    buttonState = reading;
+    
+    if (buttonState == LOW && registeredPress == 0) {
+      ledState = ((ledState == LOW) ? HIGH : LOW);
+      registeredPress = 1;
+      // set the LED using the state of the button:
+      if (ledState == HIGH) {
+        mute();
+      } else {
+        unmute();
+      }
+    }
+    if (buttonState == HIGH && registeredPress == 1) {
+      registeredPress = 0;
+    } 
+  }
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
+}
+
+void mute() {
+  ledState = HIGH;
+  digitalWrite(ledPin, ledState);
+  setAllValues(0);
+  Serial.println("muted");
+  readAndPrintAllValues();
+}
+
+void unmute() {
+  ledState = LOW;
+  digitalWrite(ledPin, ledState);
+  setAllValues(volume);
+  Serial.println("unmuted");
+  readAndPrintAllValues();
+}
+
+void readAndPrintAllValues() {
+  unsigned int data = -1;
+
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Select data register
+  Wire.write(0x00);
+  // Stop I2C transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+
+  // Read 1 byte of data
+  if (Wire.available() == 1)
+  {
+    data = Wire.read();
+  }
+  // Convert the data
+  float res_0 = (data / 256.0 ) * 10.0;
+
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Select data register
+  Wire.write(0x01);
+  // Stop I2C transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+
+  // Read 1 byte of data
+  if (Wire.available() == 1)
+  {
+    data = Wire.read();
+  }
+  // Convert the data
+  float res_1 = (data / 256.0 ) * 10.0;
+
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Select data register
+  Wire.write(0x02);
+  // Stop I2C transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+
+  // Read 1 byte of data
+  if (Wire.available() == 1)
+  {
+    data = Wire.read();
+  }
+  // Convert the data
+  float res_2 = (data / 256.0 ) * 10.0;
+
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Select data register
+  Wire.write(0x03);
+  // Stop I2C transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+
+  // Read 1 byte of data
+  if (Wire.available() == 1)
+  {
+    data = Wire.read();
+  }
+  // Convert the data
+  float res_3 = (data / 256.0 ) * 10.0;
+
+  // Output data to serial monitor
+  Serial.print("Resistance Channel-0 : ");
+  Serial.print(res_0);
+  Serial.println(" K");
+  Serial.print("Resistance Channel-1 : ");
+  Serial.print(res_1);
+  Serial.println(" K");
+  Serial.print("Resistance Channel-2 : ");
+  Serial.print(res_2);
+  Serial.println(" K");
+  Serial.print("Resistance Channel-3 : ");
+  Serial.print(res_3);
+  Serial.println(" K");
+}
+
+void setAllValues(int val) {
+    // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Send instruction for POT channel-0
+  Wire.write(0x00);
+  // Input resistance value, 0x80(128)
+  Wire.write(val);
+  // Stop I2C transmission
+  Wire.endTransmission();
+
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Send instruction for POT channel-1
+  Wire.write(0x01);
+  // Input resistance value, 0x80(128)
+  Wire.write(val);
+  // Stop I2C transmission
+  Wire.endTransmission();
+
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Send instruction for POT channel-2
+  Wire.write(0x02);
+  // Input resistance value, 0x80(128)
+  Wire.write(val);
+  // Stop I2C transmission
+  Wire.endTransmission();
+
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Send instruction for POT channel-3
+  Wire.write(0x03);
+  // Input resistance value, 0x80(128)
+  Wire.write(val);
+  // Stop I2C transmission
+  Wire.endTransmission();
+}
+
+
--- hardware/volume_control_board.pro
+++ hardware/volume_control_board.pro
@@ -1,4 +1,4 @@
-update=Wed 13 Mar 2019 03:03:46 PM PDT
+update=Thu 28 Mar 2019 05:25:05 PM PDT
 version=1
 last_client=kicad
 [pcbnew]
@@ -28,3 +28,13 @@ version=1
 [eeschema]
 version=1
 LibDir=
+[schematic_editor]
+version=1
+PageLayoutDescrFile=
+PlotDirectoryName=
+SubpartIdSeparator=0
+SubpartFirstId=65
+NetFmtName=
+SpiceAjustPassiveValues=0
+LabSize=50
+ERC_TestSimilarLabels=1
